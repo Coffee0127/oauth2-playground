b1ae85e0a7ece2b6c3178f35f6f076d38a56f69e
Bo-Xuan Fan <coffee0127@gmail.com>
2022-03-26 21:26:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# OAuth 2 Playground
+
+Playground for OAuth 2.0:
+
+- [LINE Login](https://developers.line.biz/en/docs/line-login/getting-started/)
+- [LINE Notify](https://notify-bot.line.me/)
---
+++ src/main/java/io/github/coffee0127/oauth2/MainApplication.java
@@ -0,0 +1,12 @@
+package io.github.coffee0127.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MainApplication.class, args);
+  }
+}
