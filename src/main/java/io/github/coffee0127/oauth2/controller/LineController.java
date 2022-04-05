3d200f07f870bcba5001be7598fa499a91878e65
Bo-Xuan Fan <coffee0127@gmail.com>
2022-04-05 09:59:41 +0800

    Inject WebSession and ServerHttpResponse directly

--- src/main/java/io/github/coffee0127/oauth2/controller/LineController.java
+++ src/main/java/io/github/coffee0127/oauth2/controller/LineController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
 
@@ -32,35 +31,24 @@ public class LineController {
   private final LineService lineService;
 
   @GetMapping("/login")
-  public Mono<Void> login(ServerWebExchange exchange) {
-    return exchange
-        .getSession()
-        .map(
-            session -> {
-              var state = UUID.randomUUID().toString();
-              var nonce = UUID.randomUUID().toString();
-              session.getAttributes().put(LINE_LOGIN_STATE, state);
-              session.getAttributes().put(LINE_LOGIN_NONCE, nonce);
-              var redirectUri = lineService.getRedirectUri(state, nonce);
-              log.debug("redirectUri = {}", redirectUri);
-              return redirectUri;
-            })
-        .flatMap(redirectUri -> RedirectUtils.redirect(exchange.getResponse(), redirectUri));
+  public Mono<Void> login(WebSession session, ServerHttpResponse response) {
+    var state = UUID.randomUUID().toString();
+    var nonce = UUID.randomUUID().toString();
+    session.getAttributes().put(LINE_LOGIN_STATE, state);
+    session.getAttributes().put(LINE_LOGIN_NONCE, nonce);
+    var redirectUri = lineService.getRedirectUri(state, nonce);
+    log.debug("redirectUri = {}", redirectUri);
+    return RedirectUtils.redirect(response, redirectUri);
   }
 
   @GetMapping("/logout")
-  public Mono<Void> logout(ServerWebExchange exchange) {
-    return exchange
-        .getSession()
-        .flatMap(
-            session ->
-                Mono.justOrEmpty(
-                        Optional.ofNullable(
-                                session.<AccessTokenResponse>getAttribute(LINE_ACCESS_TOKEN))
-                            .map(AccessTokenResponse::getAccessToken))
-                    .flatMap(lineService::revoke)
-                    .then(session.invalidate()))
-        .then(RedirectUtils.redirect(exchange.getResponse(), "/login"));
+  public Mono<Void> logout(WebSession session, ServerHttpResponse response) {
+    return Mono.justOrEmpty(
+            Optional.ofNullable(session.<AccessTokenResponse>getAttribute(LINE_ACCESS_TOKEN))
+                .map(AccessTokenResponse::getAccessToken))
+        .flatMap(lineService::revoke)
+        .then(session.invalidate())
+        .then(RedirectUtils.redirect(response, "/login"));
   }
 
   @GetMapping("/auth")
@@ -70,7 +58,8 @@ public class LineController {
       @RequestParam(value = "scope", required = false) String scope,
       @RequestParam(value = "error", required = false) String errorCode,
       @RequestParam(value = "error_description", required = false) String errorMessage,
-      ServerWebExchange exchange) {
+      WebSession session,
+      ServerHttpResponse response) {
     if (log.isDebugEnabled()) {
       log.debug("parameter code : {}", code);
       log.debug("parameter state : {}", state);
@@ -80,41 +69,31 @@ public class LineController {
     if (StringUtils.isNotBlank(errorCode) || StringUtils.isNotBlank(errorMessage)) {
       log.error("parameter errorCode : {}", errorCode);
       log.error("parameter errorMessage : {}", errorMessage);
-      return RedirectUtils.redirect(
-          exchange.getResponse(), createRedirectUri(ErrorCode.LOGIN_FAILED_CALLBACK));
+      return RedirectUtils.redirect(response, createRedirectUri(ErrorCode.LOGIN_FAILED_CALLBACK));
     }
 
-    return exchange
-        .getSession()
-        .flatMap(
-            session -> {
-              var sessionState = session.<String>getAttribute(LINE_LOGIN_STATE);
-              if (StringUtils.isNotBlank(state) && !StringUtils.equals(state, sessionState)) {
-                log.error(
-                    "Mismatch state, parameter state : {}, session state : {}",
-                    state,
-                    sessionState);
-                return RedirectUtils.redirect(
-                    exchange.getResponse(),
-                    createRedirectUri(ErrorCode.LOGIN_FAILED_MISMATCH_STATE));
-              }
+    var sessionState = session.<String>getAttribute(LINE_LOGIN_STATE);
+    if (StringUtils.isNotBlank(state) && !StringUtils.equals(state, sessionState)) {
+      log.error("Mismatch state, parameter state : {}, session state : {}", state, sessionState);
+      return RedirectUtils.redirect(
+          response, createRedirectUri(ErrorCode.LOGIN_FAILED_MISMATCH_STATE));
+    }
 
-              session.getAttributes().remove(LINE_LOGIN_STATE);
-              return lineService
-                  .getAccessToken(code)
-                  .flatMap(
-                      token -> {
-                        if (log.isDebugEnabled()) {
-                          log.debug("scope : {}", token.getScope());
-                          log.debug("access_token : {}", token.getAccessToken());
-                          log.debug("token_type : {}", token.getTokenType());
-                          log.debug("expires_in : {}", token.getExpiresIn());
-                          log.debug("refresh_token : {}", token.getRefreshToken());
-                          log.debug("id_token : {}", token.getIdToken());
-                        }
-                        session.getAttributes().put(LINE_ACCESS_TOKEN, token);
-                        return extractUserProfile(session, exchange.getResponse(), token);
-                      });
+    session.getAttributes().remove(LINE_LOGIN_STATE);
+    return lineService
+        .getAccessToken(code)
+        .flatMap(
+            token -> {
+              if (log.isDebugEnabled()) {
+                log.debug("scope : {}", token.getScope());
+                log.debug("access_token : {}", token.getAccessToken());
+                log.debug("token_type : {}", token.getTokenType());
+                log.debug("expires_in : {}", token.getExpiresIn());
+                log.debug("refresh_token : {}", token.getRefreshToken());
+                log.debug("id_token : {}", token.getIdToken());
+              }
+              session.getAttributes().put(LINE_ACCESS_TOKEN, token);
+              return extractUserProfile(session, response, token);
             });
   }
 
